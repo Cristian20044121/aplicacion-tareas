d0564922272d72092b42081e055b6551d3c26b1f
Cristian Caro <crdcaro01@gmai.com>
2023-09-04 12:59:39 -0500

    aplicacion finalizada

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import colors from 'colors'
 import {guardarDB, leerDB} from './helpers/guardarArchivo.js'
-import  {inquirerMenu, pausa, leerInput}  from './helpers/inquirer.js';
+import  {inquirerMenu, pausa, leerInput, listadoTareasBorrar,confirmar,mostrarListadoChecklist}  from './helpers/inquirer.js';
 import Tareas from './models/tareas.js';
 
 const main = async ()=>{
@@ -12,10 +12,9 @@ const main = async ()=>{
     const tareasDB = leerDB();
 
     if(tareasDB ) {
-        // establecer tareas 
-
+        
+        tareas.cargarTareasFromArray( tareasDB);
     }
-    await pausa();
     do {
         //imprimir el menu 
         opt = await inquirerMenu();
@@ -26,7 +25,29 @@ const main = async ()=>{
                 tareas.crearTarea( desc );
             break;
             case '2':
-                console.log(tareas.listadoArr);
+                tareas.listadoCompleto();
+            break;
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+            break;
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+            break;
+            case '5':
+              const ids=   await mostrarListadoChecklist(tareas.listadoArr)
+              tareas.toggleCompletadas ( ids )
+            break;
+            case '6':
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if(id !== '0') {
+                    const ok = confirmar('Estas seguro?');
+
+                    if ( ok ){
+                        tareas.borrarTarea ( id);
+                        console.log('Tarea borrada');
+                    }
+                }
+                
             break;
         }
         
